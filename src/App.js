93e84f06d969a280632908a9860cd8d4d02015dc
sadias-dreamweb <sadiatuzjohora11@gmail.com>
2022-08-05 13:47:32 -0400

    navbar correction done. Company logo changed. Language change button added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
 import Header from './pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
     </div>
   );
 }
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import pic from '../../../assets/images/03.png';
+import { Link, NavLink } from 'react-router-dom';
+import pic from '../../../assets/images/t.png';
 
 const Header = () => {
     return (
@@ -7,28 +8,41 @@ const Header = () => {
             <div className="navbar bg-gradient-to-r from-lime-200 to-gray-700 text-base-100">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <label tabindex="0" className="btn btn-ghost lg:hidden text-base-100">
+                        <label tabindex="0" className="btn btn-ghost lg:hidden text-black">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-gradient-to-b from-lime-200 to-gray-700  rounded-box w-52">
-                            <li><a>About Us</a></li>
-                            <li><a>Services</a></li>
-                            <li><a>Case Study</a></li>
-                            <li><a>Products</a></li>
-                            <li><a>Refer A Patient</a></li>
+                            <li className='pr-2'><NavLink className='text-white' to='/'>Home</NavLink></li>
+                            <li className='pr-2'><NavLink className='text-white' to='/service'>Services</NavLink></li>
+                            <li className='pr-2'><NavLink className='text-white' to='/products'>Products</NavLink></li>
+                            <li className='pr-2'><NavLink className='text-white' to='/about'>About Us</NavLink></li>
+                            <li className='pr-2'><NavLink className='text-white' to='/blogs'>Blogs</NavLink></li>
+                            <li className='pr-2'><NavLink className='text-white' to='/contact'>Contact Us</NavLink></li>
 
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl text-gray-600"> <img className='pr-2' src={pic} alt="foot logo" /> FootBalance</a>
+                    <div className="navbar-start">
+                        {/* <a className="btn btn-ghost normal-case text-xl text-gray-600"> <img className='pr-2' src={pic} alt="foot logo" /> FootBalance</a> */}
+                        <a className="normal-case text-xl text-gray-600"> <img className='pr-2' src={pic} alt="foot logo" /> Foot Balance <span>Technology</span></a>
+                        {/* <span className=" normal-case text-xl text-gray-600"> <img className='pr-2' src={pic} alt="foot logo" /> FootBalance</span> */}
+                    </div>
+
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
-                        <li><a>About Us</a></li>
-                        <li><a>Services</a></li>
-                        <li><a>Case Study</a></li>
-                        <li><a>Products</a></li>
-                        <li><a>Refer A Patient</a></li>
+                        <li className='pr-2'><NavLink className='text-white' to='/'>Home</NavLink></li>
+                        <li className='pr-2'><NavLink className='text-white' to='/service'>Services</NavLink></li>
+                        <li className='pr-2'><NavLink className='text-white' to='/products'>Products</NavLink></li>
+                        <li className='pr-2'><NavLink className='text-white' to='/about'>About Us</NavLink></li>
+                        <li className='pr-2'><NavLink className='text-white' to='/blogs'>Blogs</NavLink></li>
+                        <li className='pr-2'><NavLink className='text-white' to='/contact'>Contact Us</NavLink></li>
                     </ul>
+                </div>
+                <div>
+                    <select id="countries" className="btn btn-ghost">
+                        <option className='text-white bg-primary' value="en">English</option>
+                        <option className='text-white bg-primary' value="bn">Bangla</option>
+                    </select>
                 </div>
             </div>
         </div>
