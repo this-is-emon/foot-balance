f6eed9fa9b7f70f48163a42fbdad87b6d83fba23
sadias-dreamweb <sadiatuzjohora11@gmail.com>
2022-08-06 17:32:10 -0400

    we can help you walk again section created in hero1 branch, AOS animation used

--- src/pages/Home/Carousel.js
+++ src/pages/Home/Carousel.js
@@ -6,15 +6,9 @@ import pic3 from '../../../src/assets/images/ortho27.png';
 const Carousel = () => {
     return (
         <div class="carousel w-full">
-            {/* <div id="slide1" class="carousel-item relative w-full">
-                <img src={pic1} alt='slider 1 pic' class="w-full" />
-                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" class="btn btn-circle">❮</a>
-                    <a href="#slide2" class="btn btn-circle">❯</a>
-                </div>
-            </div> */}
             <div id="slide2" class="carousel-item relative w-full">
                 <img src={pic2} alt='slider 2 pic' class="w-full" />
+                <p>test</p>
                 <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" class="btn btn-circle">❮</a>
                     <a href="#slide3" class="btn btn-circle">❯</a>
