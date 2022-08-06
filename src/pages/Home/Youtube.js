a10b47d7ef799cf5d0680c86783aeb4dd8cb9d62
sadias-dreamweb <sadiatuzjohora11@gmail.com>
2022-08-06 17:47:53 -0400

    working on adding outube video

---
+++ src/pages/Home/Youtube.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Youtube = () => {
+    return (
+        <div>
+            <iframe
+                src="https://www.youtube.com/watch?v=1uXbBusjUH0&t=1s"
+                frameborder="0"
+                allow="autoplay; encrypted-media"
+                allowfullscreen
+                title="video"
+            />{" "}
+        </div>
+    );
+};
+
+export default Youtube;
