64a1611ab425f5366cb2d7aa81bbff85b28946a5
sadias-dreamweb <sadiatuzjohora11@gmail.com>
2022-08-21 11:43:30 -0400

    award page is under process, pictures are added

--- src/pages/Awards/Awards.js
+++ src/pages/Awards/Awards.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import pic1 from '../../assets/images/cer1.jpeg'
+import pic2 from '../../assets/images/cer2.jpeg'
+import pic3 from '../../assets/images/cer3.jpeg'
+import pic4 from '../../assets/images/cer4.jpeg'
 
 const Awards = () => {
     return (
         <div>
-
+            <div class="grid grid-cols-4 gap-4">
+                <div>
+                    <img src={pic1} alt='certificate1 pic' />
+                </div>
+                <div>
+                    <img src={pic2} alt='certificate2 pic' />
+                </div>
+                <div>
+                    <img src={pic3} alt='certificate3 pic' />
+                </div>
+                <div>
+                    <img src={pic4} alt='certificate4 pic' />
+                </div>
+            </div>
         </div>
     );
 };
