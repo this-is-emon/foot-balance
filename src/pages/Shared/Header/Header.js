40ba89f0ad2e7d4593d4cce2a6e59f8ca1852aa4
Mofijul Islam Emon <53686083+this-is-emon@users.noreply.github.com>
2022-08-22 18:29:58 +0600

    changing different button to 'useTranslation()' hook
    -- Destructure 't' function from useTranslation() hook.
    -- Wrap the Heder elements name into 't()' functiion.

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { NavLink } from 'react-router-dom';
 import pic from '../../../assets/images/t.png';
 
 const Header = () => {
+
+    const {t} = useTranslation();
+
     return (
         <div>
             <div className="navbar bg-gradient-to-r from-lime-200 to-gray-700 text-base-100 lg:fixed lg:top-0 z-50">
@@ -12,12 +16,12 @@ const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-gradient-to-b from-lime-200 to-gray-700  rounded-box w-52">
-                            <li className='pr-2'><NavLink className='text-white' to='/'>Home</NavLink></li>
-                            <li className='pr-2'><NavLink className='text-white' to='/service'>Services</NavLink></li>
-                            <li className='pr-2'><NavLink className='text-white' to='/products'>Products</NavLink></li>
-                            <li className='pr-2'><NavLink className='text-white' to='/about'>About Us</NavLink></li>
-                            <li className='pr-2'><NavLink className='text-white' to='/blogs'>Blogs</NavLink></li>
-                            <li className='pr-2'><NavLink className='text-white' to='/contact'>Contact Us</NavLink></li>
+                            <li className='pr-2'><NavLink className='text-white' to='/'>{t("navbar.home")}</NavLink></li>
+                            <li className='pr-2'><NavLink className='text-white' to='/service'>{t("navbar.services")}</NavLink></li>
+                            <li className='pr-2'><NavLink className='text-white' to='/products'>{t("navbar.products")}</NavLink></li>
+                            <li className='pr-2'><NavLink className='text-white' to='/about'>{t("navbar.aboutUs")}</NavLink></li>
+                            <li className='pr-2'><NavLink className='text-white' to='/blogs'>{t("navbar.blogs")}</NavLink></li>
+                            <li className='pr-2'><NavLink className='text-white' to='/contact'>{t("navbar.contactUs")}</NavLink></li>
 
                         </ul>
                     </div>
@@ -31,12 +35,12 @@ const Header = () => {
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
-                        <li className='pr-2'><NavLink className='text-white' to='/'>Home</NavLink></li>
-                        <li className='pr-2'><NavLink className='text-white' to='/services'>Services</NavLink></li>
-                        <li className='pr-2'><NavLink className='text-white' to='/products'>Products</NavLink></li>
-                        <li className='pr-2'><NavLink className='text-white' to='/about'>About Us</NavLink></li>
-                        <li className='pr-2'><NavLink className='text-white' to='/blogs'>Blogs</NavLink></li>
-                        <li className='pr-2'><NavLink className='text-white' to='/contact'>Contact Us</NavLink></li>
+                        <li className='pr-2'><NavLink className='text-white' to='/'>{t("navbar.home")}</NavLink></li>
+                        <li className='pr-2'><NavLink className='text-white' to='/services'>{t("navbar.services")}</NavLink></li>
+                        <li className='pr-2'><NavLink className='text-white' to='/products'>{t("navbar.products")}</NavLink></li>
+                        <li className='pr-2'><NavLink className='text-white' to='/about'>{t("navbar.aboutUs")}</NavLink></li>
+                        <li className='pr-2'><NavLink className='text-white' to='/blogs'>{t("navbar.blogs")}</NavLink></li>
+                        <li className='pr-2'><NavLink className='text-white' to='/contact'>{t("navbar.blogs")}</NavLink></li>
                     </ul>
                 </div>
                 <div>
